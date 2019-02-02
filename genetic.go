8d4f4d32535091cff9c1670d9f58cee61598c746
Luis P. Mendes <luislupe@gmail.com>
2019-02-02 23:33:46 +0000

    Evaluation of a chromossome's genes

--- genetic.go
+++ genetic.go
@@ -16,7 +16,9 @@ var universe = make(map[int]popDetails)
 const maxPrice = 10000
 const univSize = 100
 const popSize = 8
-const indivSize = 10 // 10 products, means 20 aleles
+const indivSize = 10 				// 10 products, means 20 aleles
+const kitPrice = 9000				// in cents
+const familyRatio = 7.0/10.0		// at least 7 families in 10 products
 
 func rndInt(lo, hi int) int {
 	return rand.Intn(hi-lo) + lo
@@ -45,13 +47,11 @@ func randEnabled() int {
 func buildGene(codes []int) []int {
 	var g []int
 	var rndC int
-	// i := 0
 	for {
 		rndC = codes[rndInt(0, len(codes))]
 		if !contains(g, rndC) {
 			g = append(g, randEnabled())
 			g = append(g, rndC)
-	// 		i++
 		}
 		if len(g) == indivSize*2 {
 			break
@@ -95,11 +95,75 @@ func buildInitialPopulation(sz int, all []int) [][]int {
 // chooseCodes converts all codes that are keys of the universe
 // and make a slice of them
 func chooseCodes(sz int) []int {
-	allCodes := make([]int, univSize)
+	allCodes := make([]int, 0)
 	for key := range universe {
 		allCodes = append(allCodes, key)
 	}
 	return allCodes
+}
+
+// quantifyGene takes a chromossome like
+// [0 0 0 70026 0 31229 0 67967 1 86397 0 6327 4 1 87620 1 14976 0 98039 1 90378]
+// and calculates its fitness
+func quantifyGene(ch []int) (int, map[int]int) {
+	active := false
+	price := 0
+	families := make(map[int]int)
+	for i := 0; i < len(ch); i++ {
+		if i%2 == 0 && ch[i] == 1 {
+			active = true
+		}
+		if i%2 == 1 && active {
+			u := universe[ch[i]]
+			price += u.price
+			family := u.family
+			families[family] += 1
+		}
+	}
+	return price, families
+}
+
+// calculateFitness takes the sum of prices of the products on each gene,
+// in cents, and the map of nr. of products per family and calculates the
+// fitness according to:
+// 1. the distance from p to 9000.  Should not be lower
+// 2. the product should be widespread per family
+func calculateFitness(p int, f map[int]int) float64 {
+	nrProducts := 0
+	fit := 0.0
+	factor := 500.0
+	penalty := 2.0
+    switch {
+		case p > kitPrice:
+				fit -= float64(p - kitPrice)
+		case p < kitPrice:
+				fit -= penalty * float64(kitPrice - p)
+	}
+	fmt.Println("p: ", p, "  f: ", f, "  fit: ", fit)
+
+	nrFamilies := len(f)
+	for _, v := range f {
+		nrProducts += v
+	}
+
+	fit += factor * float64(nrProducts) / float64(nrFamilies)
+	
+
+	fmt.Println("p: ", p, "  f: ", f, "  fit: ", fit, "  nrProducts: ", nrProducts, " nrFamilies: ", nrFamilies)
+	return fit
+}
+
+func evaluateChromossome(pop [][]int) map[int]float64 {
+	fit := make(map[int]float64)
+	p := 0
+	f := make(map[int]int)
+	for i := 0; i < len(pop); i++ {
+		p, f = quantifyGene(pop[i])
+		fmt.Println("Gene: ", pop[i])
+		fit[i] = calculateFitness(p, f)
+
+	}
+	return fit
 }
 
 func main() {
@@ -108,7 +172,9 @@ func main() {
 	universe = mapUniverse(univSize)
 	fmt.Println("Universe: ", universe)
 	univCodes := chooseCodes(popSize)
+	fmt.Println("univCodes: ", univCodes)
 	population := buildInitialPopulation(popSize, univCodes)
 	fmt.Println("Population: ", population)
+	evaluateChromossome(population)
 
 }
